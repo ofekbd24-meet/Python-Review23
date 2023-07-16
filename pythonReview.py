a8c44389b4e7d162788013c3c597d73ff84a7396
ofek <ofekbendavid9@gmail.com>
2023-07-16 16:37:31 +0300

    done :D

---
+++ pythonReview.py
@@ -0,0 +1,36 @@
+# this disctionary has the functionality of a class
+# instead you can write it as a class easily
+def create_youtube_video(title: str, description: str, hashtags: list) -> dict:
+    video = {
+        "title": title,
+        "description": description,
+        "likes": 0,
+        "dislikes": 0,
+        "comments": [],
+        "hashtag": hashtags[0:5]
+    }
+    return video
+
+def like(youtube_video: dict) -> dict:
+    youtube_video["likes"] += 1
+    return youtube_video
+
+def dislike(youtube_video: dict) -> dict:
+    youtube_video["dislikes"] += 1
+    return youtube_video
+
+def add_comment(youtube_video: dict, username: str, comment_text: str) -> dict:
+    youtube_video["comments"].append({"username": username, "comment": comment_text})
+    return youtube_video
+
+def similarity_to_video(video_a: dict, video_b: dict) -> float:
+    pct = 0
+    return [(pct := pct + 0.2)if h in video_b["hashtag"]else(pct) for h in video_a["hashtag"]][-1]
+
+video = create_youtube_video("test", "example", ["a", "b", "c", "d", "e", "f"])
+for i in range(495):
+    like(video)
+print(video)
+video_b = create_youtube_video("example", "test", ["c", "d", "e", "f", "g"])
+print(video_b)
+print(similarity_to_video(video, video_b))
